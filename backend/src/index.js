e980c3fce0c1bfffce81b5a683a44164c869a368
Leandro_Wendisch <126110985+Le0-W-L@users.noreply.github.com>
2024-10-15 19:34:19 -0300

    Revert "ATUALIZAÇÃO 15-10-2024"

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,5 @@
 const express = require('express');
-const sequelize = require('./config/database')
-
 const app = express();
-
-async function conn() {
-
-    try {
-        await sequelize.authenticate();
-        console.log('Conexão estabelecida com sucesso.')
-    }
-
-    catch (error) {
-        console.error('Erro na conexão: ', error)
-    }
-
-}
-conn();
-
 app.listen(3000, () =>{
     console.log('Servidor Funcionando');
 })
