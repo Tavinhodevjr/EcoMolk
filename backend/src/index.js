6487117709904e2dcc610620d69049d1642084be
Le0-W-L <leandro.lanzoni0400@gmail.com>
2024-10-18 15:17:03 -0300

    ATUALIZAÇÃO 18/10/2024
    - Instalação do módulo express-session para controle de cookies e sessões;
    - Implementei sistema de Login(simplificado).

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,19 @@ const cors = require('cors');
 const sequelize = require('./config/database');
 const Users = require('./models/users');
 // const Residuos = require('./models/residuos');
+const session = require('express-session')
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.text());
 app.use(bodyParser.json());
 app.use(cors());
+app.use(session({
+    secret: 'chave-secreta',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false } // FALSE para DESENVOLVIMENTO ------ TRUE para PRODUÇÃO
+}))
 
 //MÉTODO DE CONEXÃO COM O BANCO DE DADOS
 async function conn() {
@@ -33,7 +40,7 @@ conn();
 app.post('/cadastrar', async (req, res) => { 
 
     try {
-        //PEGA OS DADOS DO CORPO DA REQUISIÇÃO
+        //SOLICITA OS DADOS DO CORPO DA REQUISIÇÃO
         const { nome, email, senha, telefone, nome_empresa } = req.body
 
         //CRIANDO NOVO USUÁRIO COM SEQUELIZE
@@ -46,12 +53,40 @@ app.post('/cadastrar', async (req, res) => {
         })
 
         //RETORNA UMA MENSAGEM DE SUCESSO EM JSON
-        res.status(201).json({ message: 'Usuário cadastrado com sucesso.', usuario: newUser })
+        return res.status(201).json({ message: 'Usuário cadastrado com sucesso.', usuario: newUser })
     } 
 
     catch (error) {
         //RETORNA UMA MENSAGEM DE ERRO
-        res.status(500).json({ message: 'Erro ao cadastrar o usuário', error: error.message })
+        return res.status(500).json({ message: 'Erro ao cadastrar o usuário', error: error.message })
+    }
+})
+
+//ROTA PARA A VERIFICAÇÃO DO LOGIN                        **IMPORTANTE: SEMPRE QUE FOR ENVIAR MAIS DE UMA RESPOSTA POR REQUISISÃO**
+app.post('/login', async (req, res) => {              //     **ADICIONAR RETURN NAS RESPOSTAS PARA ENCERRAR O FLUXO DE EXECUÇÃO**
+
+    try {
+        //SOLICITA OS DADOS NO CORPO DA REQUISIÇÃO
+        const { email, senha } = req.body
+
+        //VERIFICA SE O EMAIL E SENHA ESTÃO NO BANCO DE DADOS
+        const user = await Users.findOne({ where: { email, senha } })
+
+        //VERIFICA AS CREDENCIAIS
+        if(!user){
+            return res.status(404).json({ message: 'Credenciais inválidas.' })
+        }
+        
+        //ARMAZENA O ID DO USUÁRIO NA SESSÃO
+        req.session.userId = user.id
+
+        //MENSAGEM DE CONFIRMAÇÃO DO SISTEMA
+        return res.status(200).json({ message: 'Login bem sucedido.', usuario: user })
+    } 
+    
+    catch (error) {
+        //RETORNA UMA MENSAGEM DE ERRO
+        return res.status(500).json({ message: 'Erro ao tentar fazer login', error: error.message })
     }
 })
 
