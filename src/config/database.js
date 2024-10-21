183ebac63dedb0f73998b4f4cfc9d5bd2a8703b6
Tavinhodevjr <otaviopampolha@gmail.com>
2024-10-21 20:06:03 -0300

    Projeto pronto para conectar com o banco

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('bdEcoMolk', 'root', 'Ot2004**', {
+const sequelize = new Sequelize('bdecomolk', 'root', 'Ot2004**', {
     host: 'localhost',
     dialect: 'mysql'
 })
