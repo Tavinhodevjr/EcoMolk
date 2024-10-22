c5b31eb6b16eee204c96d4545efb0dfa31ec716c
laurabonilha <bonilha.laura@gmail.com>
2024-10-22 19:35:56 -0300

    Alterações na tela de cadastro (que me conectar) para conexão com backend

---
+++ src/views/QueroConectar/conectar_script.js
@@ -0,0 +1,40 @@
+document.getElementById('registerForm').addEventListener('submit', async function(e) {
+    e.preventDefault(); 
+
+    // Coleta dos dados do formulário
+    const nome = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+    const nome_empresa = document.getElementById('empresa').value;
+    const telefone = document.getElementById('telefone').value;
+    const residuo = document.getElementById('residuo').value; // Ainda não está no db
+
+    try {
+        // Requisição ao back-end
+        const response = await fetch('http://localhost:3000/cadastrar', { // Verificar sempre o caminho correto
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                nome,
+                email,
+                senha,
+                telefone,
+                nome_empresa,
+                residuo  // Incluído no corpo da requisição - não está no db
+            })
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            alert('Usuário cadastrado com sucesso!');
+        } else {
+            alert('Erro: ' + result.message);
+        }
+    } catch (error) {
+        console.error('Erro ao enviar dados:', error);
+        alert('Erro ao tentar cadastrar o usuário.');
+    }
+});
